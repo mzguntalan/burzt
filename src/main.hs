7795368bdc79d7c69ae68e66742f213eb62db638
mzguntalan <mzguntalan@gmail.com>
2024-12-26 15:36:40 +0900

    Added a point type

--- src/main.hs
+++ src/main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-type VectorGraphic = Float -> (Float, Float)
 
 class Vector a where 
     (+) :: a -> a -> a 
@@ -8,7 +7,29 @@ class Vector a where
     (*) :: Float -> a -> a
     negate :: a -> a
 
+type Point = (Float, Float)  
+addPoint :: Point -> Point -> Point
+addPoint (x, y) (r, s) = (x Prelude.+ r, y Prelude.+ s)
 
+subtractPoint :: Point -> Point -> Point
+subtractPoint (x, y) (r, s) = (x Prelude.- r, y Prelude.- s)
+
+scalePoint :: Float -> Point -> Point 
+scalePoint s (x, y) = (s Prelude.* x, s Prelude.* y)
+
+negatePoint :: Point -> Point 
+negatePoint (x, y) = (Prelude.negate x, Prelude.negate y)
+
+instance Vector Point where
+    (+) = addPoint
+    (-) = subtractPoint
+    (*) = scalePoint
+    negate = negatePoint
+
+
+
+
+type VectorGraphic = Float -> (Float, Float)
 vectorGraphicParameter :: Float -> Float
 vectorGraphicParameter x 
     | x >= 0 = x Prelude.- fromIntegral (floor x) :: Float
@@ -26,7 +47,22 @@ subtractVectorGraphic :: VectorGraphic -> VectorGraphic -> VectorGraphic
 subtractVectorGraphic v w = addVectorGraphic v (negateVectorGraphic w)
 
 scaleVectorGraphic :: Float -> VectorGraphic -> VectorGraphic
-scaleVectorGraphic s v t = (s Prelude.* x, s Prelude.* y) where (x, y) = v t 
+scaleVectorGraphic s v t = (s Prelude.* x, s Prelude.* y) where (x, y) = v t
+
+instance Vector VectorGraphic where
+    (+) = addVectorGraphic
+    (-) = subtractVectorGraphic
+    (*) = scaleVectorGraphic
+    negate = negateVectorGraphic
+
+makeCurve :: Point -> VectorGraphic
+makeCurve = const 
+-- makeCurve :: Point -> Point -> VectorGraphic
+-- makeCurve :: Point -> Point -> Point -> VectorGraphic
+
+
+
+
 
 main :: IO()
 main = putStrLn "Hello World"
