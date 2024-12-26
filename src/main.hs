c943b299f5634a9e1a66276daa5e0d7a6a9aa241
mzguntalan <mzguntalan@gmail.com>
2024-12-26 12:22:29 +0900

    VectorGraphic type and Vector class and some Operations
    operations: add, negate, subtract for VectorGraphic type

--- src/main.hs
+++ src/main.hs
@@ -1,2 +1,29 @@
+module Main where
+
+type VectorGraphic = Float -> (Float, Float)
+
+class Vector a where 
+    (+) :: a -> a -> a 
+    (-) :: a -> a -> a
+    (*) :: Float -> a -> a
+    negate :: a -> a
+
+
+vectorGraphicParameter :: Float -> Float
+vectorGraphicParameter x 
+    | x >= 0 = x Prelude.- fromIntegral (floor x) :: Float
+    | otherwise = vectorGraphicParameter (Prelude.negate x)
+
+addVectorGraphic :: VectorGraphic -> VectorGraphic -> VectorGraphic
+addVectorGraphic v w t = case () of 
+    _ | vectorGraphicParameter t <= 0.5 -> v t
+      | otherwise -> w t
+
+negateVectorGraphic :: VectorGraphic -> VectorGraphic 
+negateVectorGraphic v = v . Prelude.negate  
+
+subtractVectorGraphic :: VectorGraphic -> VectorGraphic -> VectorGraphic
+subtractVectorGraphic v w = addVectorGraphic v (negateVectorGraphic w)
+
 main :: IO()
 main = putStrLn "Hello World"
